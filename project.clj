8d822e36c3ed72ae7a3c424e03372452c805d462
Matthias Splieth <matthias@splieth.com>
2019-11-13 23:01:26 +0100

    Now with https support on graal #yay

--- project.clj
+++ project.clj
@@ -3,14 +3,15 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.martinklepsch/clj-http-lite "0.4.1"]
                  [cheshire "5.9.0"]]
   :plugins [[io.taylorwood/lein-native-image "0.3.1"]]
   :native-image {:name "circlecli"
                  :opts ["--initialize-at-build-time"
                         "--report-unsupported-elements-at-runtime"
-                        "--enable-url-protocols=http"]}
+                        "--enable-url-protocols=http"
+                        "--enable-url-protocols=https"]}
   :main circlecli.main
   :global-vars {*warn-on-reflection* true}
   :repl-options {:init-ns circlecli.main}
--- src/circlecli/main.clj
+++ src/circlecli/main.clj
@@ -3,17 +3,8 @@
             [cheshire.core :as json])
   (:gen-class))
 
-(def url "http://dev.markitondemand.com/MODApis/Api/v2/Quote/json?symbol=%s")
-
-(defn get-stock-value-of
-  [symbol]
-  (let [response (-> (format url symbol)
-                     (client/get)
-                     (:body)
-                     (json/parse-string true))]
-    {:name (response :Name) :price (response :LastPrice)}))
-
 (defn -main
   [& args]
-  (println "Joscha hat gute Ideen aber nein")
-  )
+  (System/setProperty "java.library.path"
+                      (str (System/getenv "GRAALVM_HOME") "/jre/lib"))
+  (println (client/get "https://google.com")))
